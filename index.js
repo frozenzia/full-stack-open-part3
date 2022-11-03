0b3e259d9516e37beb684414f4044eab41abf855
Paul Vinson Brown <paul.brown@mukavait.fi>
2022-11-03 07:15:48 +0200

    Add backup PORT as 5001

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/api/persons', (req, res, next) => {
   }
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
